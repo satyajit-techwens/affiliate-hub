9f976dba7d20ba02141297a7584b9972c8e40de8
satyajit-techwens <satyajit.ghosh@techwens.in>
2024-10-30 18:36:34 +0530

    escape html

--- templates/settings/impact.php
+++ templates/settings/impact.php
@@ -2,8 +2,8 @@
     // print_r($credentials);
 ?>
     <label for="account_id">Account ID:</label>
-    <input type="text" id="account_id" name="account_id" value="<?php echo (!empty($credentials["account_id"]))?$credentials["account_id"]:""; ?>" required>
+    <input type="text" id="account_id" name="account_id" value="<?php echo (!empty($credentials["account_id"]))?esc_html($credentials["account_id"]):""; ?>" required>
 
     <label for="auth_token">Auth Token:</label>
-    <input type="password" id="auth_token" name="auth_token" value="<?php echo (!empty($credentials["auth_token"]))?$credentials["auth_token"]:""; ?>" required>
+    <input type="password" id="auth_token" name="auth_token" value="<?php echo (!empty($credentials["auth_token"]))?esc_html($credentials["auth_token"]):""; ?>" required>
 
